eda9da5fe23910d423257da0fe2250724ba0732e
3A532077 <k0982637445@gmail.com>
2019-05-06 02:13:12 +0800

    預約分為透過診所以及透過醫生

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -2,14 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Doctor;
+use App\Reservation;
 use App\Section;
+use App\Staff;
 use Illuminate\Http\Request;
 
 class ReservationController extends Controller
 {
-    public function index(){
-        $sections	=Section::orderBy('date')->get();
-        $data	=	['sections'=> $sections];
+    public function index($id){
+        $sections = Section::where('clinic_id',$id)->orderBy('date')->get();
+        $doctors = Doctor::all();
+        $staff = Staff::all();
+        $data	=	['sections'=> $sections,'staff'=>$staff,'doctors'=>$doctors];
         return view('reservation2',$data);
     }
+    public function index2($id){
+        $sections = Section::where('doctor_id',$id)->orderBy('date')->get();
+        $doctors = Doctor::all();
+        $staff = Staff::all();
+        $data	=	['sections'=> $sections,'staff'=>$staff,'doctors'=>$doctors];
+        return view('reservation2',$data);
+    }
+
+    /**
+     * @param $id
+     */
+    public function store($id){
+        $sections = Section::find($id);
+        $reservation = Reservation::where('section_id',$id)->count();
+        Reservation::create([
+            'section_id' => $id,
+            'member_id' => auth()->user()->id,
+            'number' => $reservation+1,
+            'date' => $sections->date,
+        ]);
+        return view('welcome');
+    }
+
+
 }
