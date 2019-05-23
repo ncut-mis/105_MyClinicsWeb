5c3008e7fe334acafe918bde8c5c746aad3b10b0
3A532077 <k0982637445@gmail.com>
2019-05-23 14:53:12 +0800

    掛號資料表合併原預約資料表

---
+++ database/migrations/2018_12_14_063852_create_registers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRegistersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('section_id');
+            $table->unsignedInteger('member_id');
+            $table->string('create_at');
+            $table->string('reservation_no');
+            $table->string('reminding_time');
+            $table->string('reminding_no');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('register');
+    }
+}
